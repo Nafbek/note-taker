2c5e7093d468b3a67333cfa1802c605e7cb87e91
Bekam <mspbekan@gmail.com>
2023-05-19 17:36:36 -0500

    the delete request modified with a correct data conversion

--- routes/notes.js
+++ routes/notes.js
@@ -4,9 +4,6 @@ const shortid = require('shortid')
 
 
 // //write file and create api
-// * GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-
-// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you?????????????????????????????????????????).
 
 notes.get('/notes', (req, res)=>{
     readFromFile('./db/db.json')
@@ -40,14 +37,16 @@ notes.delete('/notes/:id', (req, res)=> {
 
     readFromFile('./db/db.json')
 
-    console.log(readFromFile('./db/db.json'))
+
     .then((data)=> {
-        const notes = JSON.parse(data)
-        const notesIndex = notes.findIndex((note)=>note.id ===noteId)
+        // console.log("data form file", data)
+        const parsedNotes = JSON.parse(data)
+      
+        const notesIndex = parsedNotes.findIndex((note)=>note.id ===noteId)
 
         if(notesIndex !== -1) {
-            notes.splice(notesIndex, 1)
-              writeToFile('./db/db.json', notes)
+            parsedNotes.splice(notesIndex, 1)
+              writeToFile('./db/db.json', JSON.stringify(parsedNotes))
         .then(()=> res.json({update: 'Your note has been deleted'}))
         .catch((err)=> console.log(err))
         }else {
