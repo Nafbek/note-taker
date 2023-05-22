eb3800c0ed7013d4a7780bce055d2c69fd72c85b
Bekam <mspbekan@gmail.com>
2023-05-21 22:20:17 -0500

    the port variable in the server.js modified

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const apiRoutes = require('./routes/index.js');
 const clog = require('./middleware/clogs.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 //Initialize the express app
 const app = express();
